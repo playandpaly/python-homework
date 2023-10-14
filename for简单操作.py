1f36e69f7dbf034490341d3740bbd9d3d75390f5
playandpaly <147911535+playandpaly@users.noreply.github.com>
2023-10-14 16:39:41 +0800

    Update for简单操作.py

--- for简单操作.py
+++ for简单操作.py
@@ -4,6 +4,6 @@ for i in list1:
     list2 += i
     list2.extend([i])
 print(list2)
-print(list2, end='')  # end换行end=''换行变成换空格
+print(list2, end='')  #  end换行end=''换行变成换空格
 for i in range(1,10):
     print(i)
