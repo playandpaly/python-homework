bffbc01714879154a505e05a5b8dffae9775cbf4
playandpaly <147911535+playandpaly@users.noreply.github.com>
2023-10-14 16:37:44 +0800

    Add files via upload

---
+++ for简单操作.py
@@ -0,0 +1,9 @@
+list1 = list('曾许人间第一流')
+list2 = []
+for i in list1:
+    list2 += i
+    list2.extend([i])
+print(list2)
+print(list2, end='')  # end换行end=''换行变成换空格
+for i in range(1,10):
+    print(i)
